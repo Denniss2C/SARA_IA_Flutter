67893ec1da82908b70fcfaf8961e5b635cb284d0
Denniss2C <denniscaisa@gmail.com>
2025-03-14 11:05:49 -0500

    First commit create app
    Se creó un nuevo app con versión de java 17, para iniciar con el desarrollo

---
+++ .dart_tool/flutter_build/f8c418447feaa9eb86832454d1337fa4/install_code_assets.d
@@ -0,0 +1,1 @@
+ /Users/denniscaisa/Desktop/DENNIS/MIES/Movil/SARA_IA_Flutter/.dart_tool/flutter_build/f8c418447feaa9eb86832454d1337fa4/native_assets.json: 
---
+++ android/app/src/main/kotlin/com/example/app_sara/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_sara
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
